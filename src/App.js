116417e2b63f238daaeaee455ce3dfa03c6f6637
David Bing <davidb.devops@gmail.com>
2020-12-07 15:35:49 -0600

    fix auth and route imports in App.js and index.js

--- src/App.js
+++ src/App.js
@@ -3,29 +3,27 @@ import { Login } from './components/auth/Login'
 import { Register } from './components/auth/Register'
 import './App.css';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      Astronomy Photo of the Day    
-    </div>
-  );
-}
+<>
+
+  <Route render={ () => {
+    // The user id is saved under the key app_user_id in local Storage. Change below if needed!
+      if (localStorage.getItem("app_user_id")) {
+          return (
+              <>
+                  //Components that are rendered when the user is authenticated go inside this React fragment
+              </>
+          )
+      } else {
+          return <Redirect to="/login" />
+      }
+  } } />
+
+  <Route path="/login" render={props => <Login {...props} />} />
+  <Route path="/register" render={props => <Register {...props} />} />
+</>
+  ) }
 
 export default App;
 
-{/* <Route render={() => {
-          // The user id is saved under the key app_user_id in local Storage. Change below if needed!
-            if (localStorage.getItem("app_user_id")) {
-                return (
-                    <>
-                        //Components that are rendered when the user is authenticated go inside this React fragment
-                    </>
-                )
-            } else {
-                return <Redirect to="/login" />
-            }
-        }} />
-
-        <Route path="/login" render={props => <Login {...props} />} />
-        <Route path="/register" render={props => <Register {...props} />} />
-    </> */}
