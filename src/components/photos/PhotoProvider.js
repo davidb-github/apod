cb1326e8d468314d7879e9eb38304a909fb2d4c3
David Bing <davidb.devops@gmail.com>
2020-12-08 15:28:48 -0600

    update module: PhotoProvider.js - update getApod component

--- src/components/photos/PhotoProvider.js
+++ src/components/photos/PhotoProvider.js
@@ -1,23 +1,26 @@
 //create empty array, fetch the API information, then make copy of that empty array.
-import { useState } from "react"
-import { keys } from "../Settings.js"
+import React, { useState } from "react"
+import { keys } from "../../Settings.js"
 
 
-export const PhotosContext = React.createContext()
-const [apod, setPhoto] = useState({})
+export const ApodContext = React.createContext()
 
-const getApod = () => {
-    return fetch("https://api.nasa.gov/planetary/apod?api_key=Y2aNOUmlb87yeRVuHaUBfwf0L25jF5H6rVqEO6bO")
-        .then((response) => response.json())
-        .then(setPhoto)
-}
+export const ApodProvider = (props) => {
+    const [apod, setApod] = useState({})
 
-return (
-    <PhotosContext.Provider value={
-        {
-            apod
-        }
-    }>
-        {props.children}
-    </PhotosContext.Provider>
-)
+    const getApod = () => {
+        return fetch("https://api.nasa.gov/planetary/apod?api_key=Y2aNOUmlb87yeRVuHaUBfwf0L25jF5H6rVqEO6bO")
+            .then((response) => response.json())
+            .then(setApod)
+    }
+
+    return (
+        <ApodContext.Provider value={
+            {
+                apod, setApod, getApod
+            }
+        }>
+            {props.children}
+        </ApodContext.Provider>
+    )
+} 
