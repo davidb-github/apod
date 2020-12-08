38f3d6b65cbe1d409f11e05180b1863254b2cd80
David Bing <davidb.devops@gmail.com>
2020-12-08 11:21:19 -0600

    new module: Home.js - initial commit - landing page for apod app

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+// imports
+import React from 'react'
+
+export const Home = () => {
+    
+    return (
+    <> 
+    <h1>Welcome to Astronomy Photo of the Day</h1>
+    <p>build additional home components</p>
+    </>
+    )   
+}
+
