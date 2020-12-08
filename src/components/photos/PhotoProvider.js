288774f1b88e8b4cead1fc987826d14157694543
David Bing <davidb.devops@gmail.com>
2020-12-08 14:30:41 -0600

    update module: ProtoProvider.js - implement getPhoto component

--- src/components/photos/PhotoProvider.js
+++ src/components/photos/PhotoProvider.js
@@ -1,2 +1,23 @@
 //create empty array, fetch the API information, then make copy of that empty array.
+import { useState } from "react"
 import { keys } from "../Settings.js"
+
+
+export const PhotosContext = React.createContext()
+const [apod, setPhoto] = useState({})
+
+const getApod = () => {
+    return fetch("https://api.nasa.gov/planetary/apod?api_key=Y2aNOUmlb87yeRVuHaUBfwf0L25jF5H6rVqEO6bO")
+        .then((response) => response.json())
+        .then(setPhoto)
+}
+
+return (
+    <PhotosContext.Provider value={
+        {
+            apod
+        }
+    }>
+        {props.children}
+    </PhotosContext.Provider>
+)
