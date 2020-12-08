8cbfbe2b0128341e717fa13b0b61e486948b9b85
David Bing <davidb.devops@gmail.com>
2020-12-08 15:30:55 -0600

    update module: Home.js - minor updates

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,18 @@
 // imports
-import React from 'react'
+import React, { useState, useContext, useEffect } from 'react'
+import { ApodContext } from './components/photos/PhotoProvider'
+
+// will make call to apod API and display photo
+
 
 export const Home = () => {
+   
     
     return (
     <> 
     <h1>Welcome to Astronomy Photo of the Day</h1>
     <p>build additional home components</p>
+    
     </>
     )   
 }
