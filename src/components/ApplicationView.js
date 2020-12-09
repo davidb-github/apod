184e9b2689d4561da28cf128db31708188bb1047
David Bing <davidb.devops@gmail.com>
2020-12-09 13:29:34 -0600

    add route and wire up CalendarPage.js - header page only

--- src/components/ApplicationView.js
+++ src/components/ApplicationView.js
@@ -2,6 +2,8 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { ApodProvider } from "./photos/PhotoProvider"
 import { Home } from "../Home"
+// calendar imports
+import { Calendar } from './calendar/CalendarPage'
 
 export const ApplicationViews = (props) => {
     return (
@@ -10,6 +12,13 @@ export const ApplicationViews = (props) => {
                 {/* Render the location list when http://localhost:3000/ */}
                 <Route exact path="/">
                     <Home />
+                </Route>
+            </ApodProvider>
+
+            <ApodProvider>
+                {/* Render the location list when http://localhost:3000/ */}
+                <Route path="/calendar">
+                    <Calendar />
                 </Route>
             </ApodProvider>
 
