9259c3fa05f4a59a8ad7e795f6ae5d46e2931d62
David Bing <davidb.devops@gmail.com>
2020-12-09 14:56:57 -0600

    update module: PhotoDetails.js - add/update/improve

--- src/components/photos/PhotoDetail.js
+++ src/components/photos/PhotoDetail.js
@@ -10,15 +10,33 @@ export const PhotoDetail = () => {
 
 
     useEffect(() => {
-        console.log("useEffect state:", apod)
         getApod()
+        // console.log("useEffect state:", apod)
     }, [])
 
 
     return (
         <div className="apod">
             {
-                <section className="apod__photo-details">
+                <section>
+                    <div>
+                        <p>Title: {apod.title}</p>
+                        <p>Date : {apod.date}</p>
+                    </div>
+                    <div>
+                        <img src={apod.url} alt="apod"></img>
+                    </div>
+                    <div>
+                        <p>Description: {apod.explanation}</p>
+                    </div>
+                </section>
+                // console.log("PhotoList executed and we are inside the return", apod.title) && <h1>Hello World</h1>
+            }
+        </div >
+    )
+}
+
+{/* <section className="apod__photo-details">
                     <article className="apod__title">
                         <p>Discover the cosmos! Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.</p>
                     </article>
@@ -27,33 +45,14 @@ export const PhotoDetail = () => {
                     <p>
 
                         {/* copyright: {apod.copyright} */}
-                        date: {apod.date}
-                        explanation: {apod.explanation}
+                        // date: {apod.date}
+                        // explanation: {apod.explanation}
 
-                        hdrurl: {apod.hdrurl}
-                        media_type: {apod.media_type}
-                        service_version: {apod.service_version}
-                        title: {apod.title}
-                        URL: {apod.url}
-                    </p>
+                        // hdrurl: {apod.hdrurl}
+                        // media_type: {apod.media_type}
+                        // service_version: {apod.service_version}
+                        // title: {apod.title}
+                //         URL: {apod.url}
+                //     </p>
 
-                </section>
-                // console.log("PhotoList executed and we are inside the return", apod.title) && <h1>Hello World</h1>
-
-
-
-            }
-        </div >
-    )
-}
-
-// {
-//             apod.copyright,
-//             apod.date,
-//             apod.explanation,
-//             apod.hrurl,
-//             apod.media_type,
-//             apod.service_version,
-//             apod.title,
-//             apod.url
-// }
+                // </section> */}
