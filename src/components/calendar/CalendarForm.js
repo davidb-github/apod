9f5e4312caa3aa491c99d1c619903217d4deeb18
David Bing <davidb.devops@gmail.com>
2020-12-15 15:09:14 -0600

    update module: CalendarForm.js - update addPhotoTag arg to pass an int instead of string

--- src/components/calendar/CalendarForm.js
+++ src/components/calendar/CalendarForm.js
@@ -62,7 +62,7 @@ export const CalendarForm = () => {
     const handleSaveFavs = () => {
         console.log(ApodContext)
         addPhoto(createPhotoObject())
-        .then(response => addPhotoTag({photoId: response.id, tagId: selectedTag })).catch(console.log)        
+        .then(response => addPhotoTag({photoId: response.id, tagId: parseInt(selectedTag) })).catch(console.log)        
     }
 
 
